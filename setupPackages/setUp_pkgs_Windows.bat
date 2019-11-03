b2d090f940173b92e25c850f264fdc45f60f38b8
Alejandro Torrico <a.torricomoreno@gmail.com>
2019-11-03 03:34:30 -0600

    fixed some bugs

---
+++ setupPackages/setUp_pkgs_Windows.bat
@@ -0,0 +1,19 @@
+install pip3  
+pip3 install os
+pip3 install sys
+pip3 install flask
+pip3 install flask_bootstrap
+pip3 install flask_dropzone
+pip3 install flask_uploads
+pip3 install pygeocoder
+pip3 install pandas
+pip3 install numpy
+pip3 install geocoder
+pip3 install flask_googlemaps
+pip3 install flask_googlemaps
+pip3 install PIL
+pip3 install PIL.ExifTags
+pip3 install requests
+pip3 install io
+pip3 install wget
+echo("sucess! All Packeges are installed")
